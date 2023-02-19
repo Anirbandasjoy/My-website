b4b8dfc6b1704b06548eaeafbe06827132a551af
joydas <joy600508@gmail.com>
2023-02-19 13:45:48 +0600

    wh

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
+
 import AboutContent from '../components/AboutContent'
 
 
 import Footer from '../components/Footer'
 import HeroImg2 from '../components/HeroImg2'
 import Navbar from '../components/Navbar'
+
 
 
 
